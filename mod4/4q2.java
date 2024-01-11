996547b937346eccc3b4c0b7f52129fa42c914d9
tdclogic1 <99967439+tdclogic1@users.noreply.github.com>
2024-01-10 23:31:12 -0500

    v.011020241131

--- mod4/4q2.java
+++ mod4/4q2.java
@@ -1,26 +1,105 @@
 public class 4q2 {
     public static void main(String[] args) {
 
-    String str = "a black cat sat on a table";
+/* missing loop header */
+for (int j = 0; j < 3; j++)
+//for (int j = 1; j < 3; j++)
+//for (int j = 1; j <= 3; j++)
+    {
+    for (int k = 0; k < 4; k++)
+    
+    {
+    
+    System.out.print(k);
+    
+    }
+    
+    System.out.println();
+    
+    }
+}
 
-int counter = 0;
+Q3
+public class 4q3 {
+    public static void main(String[] args) {
 
-for (int i = 0; i < str.length() - 1; i++)
+int count = 0;
+
+for (int x = 1; x <= 3; x++)
 
 {
 
-if (str.substring(i, i + 1).equals("a") &&
+/* for (int y = 0; y <= 2; y++)
 
-    !str.substring(i + 1, i + 2).equals("b"))
+B
+for (int y = 0; y < 3; y++)
+
+C
+for (int y = 2; y >= 0; y--)
+
+D
+for (int y = 3; y > 0; y--)
+
+E -  cORRECT
+for (int y = 0; y < x; y++)
+ */
 
 {
 
-counter++;
+count++;
 
 }
 
 }
 
-System.out.println(counter);
+System.out.println(count);
 }
+
+
+public String wordPlay(String word)
+
+{
+
+String str = "";
+
+for (int k = 0; k < word.length(); k++)
+
+{
+
+if (k % 3 == 0)
+
+{
+
+str = word.substring(k, k + 1) + str;
+
+}
+
+}
+
+return str;
+
+}
+
+//12345
+public static String changeStr(String str)
+
+{
+
+String result = "";
+
+for (int i = str.length() - 1; i >= str.length() / 2; i -= 2)
+
+{
+
+result += str.substring(i, i + 1);
+
+}
+
+return result;
+
+}
+
+What val
+
+
 }
