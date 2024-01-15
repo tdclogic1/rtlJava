f8e9c840938d9394f96adf221976eaeffac16e52
tdclogic1 <99967439+tdclogic1@users.noreply.github.com>
2024-01-15 09:55:19 -0500

    v.011520240955

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('startjava').addEventListener('click', function() {
+    window.location.href = '/course/java/index.html'; // Link to the program start page
+});
+
+document.getElementById('startC').addEventListener('click', function() {
+    window.location.href = '/course/c/index.html'; // Link to the program start page
+});
+document.getElementById('startpython').addEventListener('click', function() {
+    window.location.href = '/course/python/index.html'; // Link to the program start page
+});
+document.getElementById('startaifund').addEventListener('click', function() {
+    window.location.href = '/course/aifund/index.html'; // Link to the program start page
+});
+
+document.getElementById('requestInfo').addEventListener('click', function() {
+    // Implement action for requesting more information
+    alert('Information request form coming soon!');
+});
